e161d966db7e524b25601790fb670814c387fa10
emilieg <emilie.gerber@hotmail.com>
2016-04-03 08:48:25 -0700

    added function to change quote

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+var quotes = [
+{quote:'ble', person: 'name'},
+{quote:'mla', person: 'cj'},
+{quote:'cla', person: 'lauren'},
+{quote:'blA', person: 'marl'}
+];
+
+
+function getRandomInt() {
+    return Math.floor(Math.random() * (quotes.length)); 
+};
+
+
 $(document).ready(function() {
-  //code here
-});
+$('#myForm').submit(function(event) {
+  event.preventDefault();
+  //set retur of getRandomInt to a variable
+  var index = getRandomInt();
+  //set inner html of the quoteContainer p to new quote
+ $('#quoteContainer p').html(quotes[index].quote + " " + quotes[index].person);
+}) 
+
+})
+
